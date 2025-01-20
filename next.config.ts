10eca035190c9c872ff03ba0f3e47b554bab755b
ArrowMan <arrow.dev0515@gmail.com>
2025-01-20 07:43:50 -0500

    next.config.ts updated

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,24 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["www.shutterstock.com","www.flagpedia.net", "img.freepik.com"],
-  }
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "www.shutterstock.com",
+        pathname: "/**", // Match all paths
+      },
+      {
+        protocol: "https",
+        hostname: "www.flagpedia.net",
+        pathname: "/**", // Match all paths
+      },
+      {
+        protocol: "https",
+        hostname: "img.freepik.com",
+        pathname: "/**", // Match all paths
+      },
+    ],
+  },
 };
 
 export default nextConfig;
