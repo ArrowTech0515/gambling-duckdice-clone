dc8e0a7be8b0a4d79ef5b689aa62090a3fef2c38
ArrowMan <arrow.dev0515@gmail.com>
2025-01-20 09:00:42 -0500

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -21,6 +21,7 @@ export default function Home() {
         <HeroSection />
         <CryptoTicker />
         <Jackpots />
+        {/* <GameSections /> */}
         <GameSections />
         <StatsCards />
         <Footer />
